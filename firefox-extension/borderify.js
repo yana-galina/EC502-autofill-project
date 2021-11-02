9c8bab4979ce9d27eb3241d6b95fa07b8074cbd8
Yanni Pang <yanni@bu.edu>
2021-11-02 02:04:21 -0400

    simple input detection and alert extension

--- firefox-extension/borderify.js
+++ firefox-extension/borderify.js
@@ -1,2 +1,16 @@
-document.body.style.border = "5px solid red";
+document.body.style.border = "5px solid blue";
 
+function find_fields() {
+    let input_list = [];
+    let inputs = document.getElementsByTagName("input")
+    if (inputs.length > 0) {
+
+        for (let input of inputs) {
+           input_list.push((input.id) ? input.id: input.name);
+        }
+        alert(`There are ${inputs.length} input fields with these names: ${input_list}`);
+        // console.log(inputs);
+        // console.log(`There are ${inputs}`)
+    }
+}
+find_fields();
