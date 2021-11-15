7c6b5e712b3beeea31434f1ea6e7f6faaf3a3c23
yanchen01 <ychen01@bu.edu>
2021-11-15 18:15:19 -0500

    added example html and route

--- server/src/server.js
+++ server/src/server.js
@@ -7,9 +7,14 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'pages')));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
+
+app.get('/hidden_attribute', function(req, res) {
+	res.sendFile(path.join(__dirname, 'pages', 'hidden_attribute.html'));
+  
+})
 
 app.listen(port, () => {
 	console.log(`Server has started on port ${port}`);
